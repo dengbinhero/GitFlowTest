20fcf887bed9ff83e6ea5f9108f404339e27a9db
盛斌 邓 <dengbinhero@163.com>
2014-12-12 18:21:00 +0800

    Initial Commit

---
+++ SwiftTest/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftTest
+//
+//  Created by Dengshengbin on 14-12-12.
+//  Copyright (c) 2014年 Dengshengbin. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
